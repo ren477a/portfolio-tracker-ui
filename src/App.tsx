e71d5cb2b3df7622682fa09bd96e439f19e8d4e8
Efren Mercado Jr <18308986+efrenmercadojr@users.noreply.github.com>
2021-06-26 22:56:42 +0800

    Add routing and asset list

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,125 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { Button, Col, Row } from 'react-bootstrap';
+
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/asset-list">Asssets</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+        <Route path="/asset-list">
+          <AssetList />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+function AssetList() {
+  const assets = [
+    {
+      dop: new Date(),
+      code: "SMPH",
+      num_of_shares: 500,
+      buy_price: 37.80,
+      broker_fee: 100.00,
+    },
+    {
+      dop: new Date(),
+      code: "SMPH",
+      num_of_shares: 100,
+      buy_price: 37.80,
+      broker_fee: 100.00,
+    },
+    {
+      dop: new Date(),
+      code: "SMPH",
+      num_of_shares: 100,
+      buy_price: 37.80,
+      broker_fee: 100.00,
+    }
+  ];
+
+  const assetList = assets.map(asset => (
+    <tr>
+      <td>{asset.dop.toLocaleString()}</td>
+      <td>{asset.code}</td>
+      <td>{asset.num_of_shares}</td>
+      <td>{asset.buy_price}</td>
+      <td>{asset.broker_fee}</td>
+    </tr>
+  ));
+
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <h1>Assets</h1>
+        </Col>
+        <Col md={{ span: 2, offset: 6 }}>
+          <Button className="float-end">New Asset</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date of Purchase</th>
+              <th>Code</th>
+              <th># of Shares</th>
+              <th>Buy Price</th>
+              <th>Broker Fee</th>
+            </tr>
+          </thead>
+          <tbody>
+            {assetList}
+          </tbody>
+        </Table>
+      </Row>
+      
+    </Container>
+  );
+}
+
+function Dashboard() {
+  return (
+    <Container>
+      <Row>
+        <h2>Dashboard</h2>
+      </Row>
+    </Container>
+  );
+}
+
+
 
 export default App;
