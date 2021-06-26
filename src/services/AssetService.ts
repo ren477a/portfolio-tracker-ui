e147e58bbe8fe4c0038f2bf7bde56b48f46397e4
Efren Mercado Jr <18308986+efrenmercadojr@users.noreply.github.com>
2021-06-26 23:39:41 +0800

    Update assets temp data

--- src/services/AssetService.ts
+++ src/services/AssetService.ts
@@ -5,7 +5,7 @@ function getAssets(): APIResult<ReadonlyArray<Asset>> {
   const assets = [
     {
       id: 1,
-      dateOfPurchase: new Date(),
+      dateOfPurchase: new Date(2021, 1, 1),
       code: "SMPH",
       numOfShares: 500,
       buyPrice: 37.80,
@@ -13,18 +13,18 @@ function getAssets(): APIResult<ReadonlyArray<Asset>> {
     },
     {
       id: 2,
-      dateOfPurchase: new Date(),
+      dateOfPurchase: new Date(2021, 4, 1),
       code: "SMPH",
       numOfShares: 100,
-      buyPrice: 37.80,
+      buyPrice: 38.20,
       brokerFee: 100.00,
     },
     {
       id: 3,
-      dateOfPurchase: new Date(),
+      dateOfPurchase: new Date(2021, 7, 1),
       code: "SMPH",
       numOfShares: 100,
-      buyPrice: 37.80,
+      buyPrice: 38.40,
       brokerFee: 100.00,
     }
   ];
