048247c1fd54d144ec08fc97f265018ffcd82f78
Efren Mercado Jr <18308986+efrenmercadojr@users.noreply.github.com>
2021-06-27 12:05:57 +0800

    Setup tests and CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { expect } from 'chai';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import App from './App';
 
-describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
+describe('App', () => {
+  it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import { Button, Col, Row } from 'react-bootstrap';
 import AssetList from './scenes/AssetList';
-
 
 interface AppProps {}
 
@@ -33,12 +27,12 @@ function App({}: AppProps) {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-        <Route path="/asset-list">
-          <AssetList />
-        </Route>
-        <Route path="/">
-          <Dashboard />
-        </Route>
+          <Route path="/asset-list">
+            <AssetList />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
         </Switch>
       </div>
     </Router>
@@ -54,6 +48,5 @@ function Dashboard() {
     </Container>
   );
 }
-
 
 export default App;
